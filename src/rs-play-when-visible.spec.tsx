3e13c015a9e1801da8502c0ed538216a2b0cd45f
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2021-01-04 14:51:11 -0800

    Renamed to PWVSpring & added extra functioniality
    The component was renamed from PlayWhenVisible to PWVSpring, where PWV
    is an alias for PlayWhenVisible.
    
    Extra functionaltiy includes the onRest and onStart methods that are
    triggered when the animation starts and stops.
    
    Additionally, the component now uses a child function that allows the
    user to pass the animation to the style prop in any animated component.

--- src/rs-play-when-visible.spec.tsx
+++ src/rs-play-when-visible.spec.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { PlayWhenVisible } from "./rs-play-when-visible";
+import { PWVSpring } from "./rs-play-when-visible";
 import { animated } from "react-spring";
 
 describe("PlayWhenVisible", () => {
     it("should render the content from the child function", () => {
         const { getByText } = render(
-            <PlayWhenVisible>
-                {({ setupAnimationProps }) => (
-                    <animated.div
-                        {...setupAnimationProps({
-                            variants: {
-                                from: { opacity: 0 },
-                                to: { opacity: 1 },
-                            },
-                        })}
-                    >
-                        Animation!
-                    </animated.div>
+            <PWVSpring
+                animation={{
+                    from: {
+                        opacity: 0,
+                    },
+                    to: {
+                        opacity: 1,
+                    },
+                }}
+            >
+                {({ animation }) => (
+                    <animated.div style={animation}>Animation!</animated.div>
                 )}
-            </PlayWhenVisible>
+            </PWVSpring>
         );
 
         expect(getByText("Animation!")).toBeInTheDocument;
--- src/rs-play-when-visible.tsx
+++ src/rs-play-when-visible.tsx
@@ -1,61 +1,81 @@
-import React, { CSSProperties, useState } from "react";
+import React, { useState } from "react";
 import {
-    animated,
+    AnimatedValue,
     SpringConfig,
     useSpring,
     UseSpringBaseProps,
 } from "react-spring";
 import VisibilitySensor from "react-visibility-sensor";
 
-type Inputs = object & React.CSSProperties & UseSpringBaseProps;
+type AnimationVariant = object & React.CSSProperties & UseSpringBaseProps;
 
 interface SetupAnimationPropsArgs {
-    from: Inputs;
-    to: Inputs;
+    from: AnimationVariant;
+    to: AnimationVariant;
     config?: SpringConfig;
 }
 
-interface AnimationProps {
-    style: CSSProperties | undefined;
-}
-
-interface ChildrenArgs {
-    /**
-     * Sets up a play when visible animation based on the variants provided.
-     */
-    setupAnimationProps: (args: SetupAnimationPropsArgs) => AnimationProps;
-}
-
 interface PlayWhenVisibleProps {
+    /**
+     *
+     */
     animation: SetupAnimationPropsArgs;
+
     /**
      * If true, the animation plays only once.
      */
     onlyOnce?: boolean;
+
     /**
      * The props for the `VisibilitySensor` from `react-visibility-sensor`, excluding `partialVisibility` and `onChange`. Read more in the [react-visibility-sensor props documentation](https://github.com/joshwnj/react-visibility-sensor#props).
      */
+
     sensorOptions?: Omit<
         React.ComponentPropsWithoutRef<typeof VisibilitySensor>,
         "onChange" | "partialVisibility"
     >;
+
     /**
      * If true, requires that the animation children are fully visible before playing the animation. (Equivalent to `partialVisibility` from  the `VisibilitySensor` props)
      */
     requireFullVisibility?: boolean;
+
+    /**
+     * Callback for when the animation starts playing.
+     */
+    onStart?: () => void;
+
+    /**
+     * Callback for when the animation stops playing.
+     */
+    onRest?: (ds: Partial<unknown>) => void;
+
     /**
      * Callback for when the visibility of the animation is changed. (Equivalent to `onChange` from the `VisibilitySensor` props)
      */
     onVisiblityChange?: (visible: boolean) => void;
+
     /**
      * The child function that creates the animation props.
      */
-    // children: (args: ChildrenArgs) => JSX.Element;
-    children: React.ReactNode;
+    children: (result: {
+        animation: AnimatedValue<
+            Pick<
+                {
+                    from: unknown;
+                    to: unknown;
+                    config: unknown;
+                },
+                never
+            >
+        >;
+    }) => JSX.Element;
 }
 
-export const PlayWhenVisible = ({
+export const PWVSpring = ({
     animation,
+    onStart,
+    onRest,
     onVisiblityChange,
     onlyOnce,
     requireFullVisibility,
@@ -70,10 +90,12 @@ export const PlayWhenVisible = ({
     const canPlay = onlyOnce ? hasPlayed : isVisible;
     const fallbackVariant = onlyOnce ? (hasPlayed ? to : from) : from;
 
-    const finalAnimation = useSpring({
+    const spring = useSpring({
         from: from,
         to: canPlay ? to : fallbackVariant,
         config,
+        onStart,
+        onRest,
     });
 
     return (
@@ -88,7 +110,7 @@ export const PlayWhenVisible = ({
             }}
             {...sensorOptions}
         >
-            <animated.div style={finalAnimation}>{children}</animated.div>
+            {children({ animation: spring })}
         </VisibilitySensor>
     );
 };
