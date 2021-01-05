572bec8bc7c236e0754665984bc40960e47b0e4d
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2021-01-04 19:32:37 -0800

    Added IntersectionObserverRef & AnimationVariant

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,11 @@
-import { AnimatedValue } from "react-spring";
+import { AnimatedValue, UseSpringBaseProps } from "react-spring";
 
 export type AnimationResult = AnimatedValue<Pick<unknown, never>>;
+
+export type AnimationVariant = object &
+    React.CSSProperties &
+    UseSpringBaseProps;
+
+export type IntersectionObserverRef =
+    | ((node?: Element | null | undefined) => void)
+    | React.RefObject<unknown>;
