9dece4978d17febb906338de2f63110e937e5006
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2021-01-04 18:41:30 -0800

    Implemented react intersection observer

--- src/hooks/use-pwv-spring/use-pwv-spring.tsx
+++ src/hooks/use-pwv-spring/use-pwv-spring.tsx
@@ -1,3 +1,35 @@
+import React, { useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { animated, useSpring } from "react-spring";
+import { AnimationResult } from "../../types";
 import { PWVSpringProps } from "../../types/pwv-spring";
 
-export const usePWVSpring = ({}: PWVSpringProps) => {};
+export const usePWVSpring = ({
+    animation,
+    onStart,
+    onRest,
+    onlyOnce,
+}: Pick<PWVSpringProps, "animation" | "onlyOnce" | "onStart" | "onRest">): [
+    (node?: Element | null | undefined) => void,
+    AnimationResult
+] => {
+    const [ref, isVisible] = useInView();
+    const [hasPlayed, setPlayed] = useState(false);
+
+    if (isVisible && !hasPlayed) setPlayed(true);
+
+    const { from, to, config } = animation;
+
+    const canPlay = onlyOnce ? hasPlayed : isVisible;
+    const fallbackVariant = onlyOnce ? (hasPlayed ? to : from) : from;
+
+    const spring = useSpring({
+        from: from,
+        to: canPlay ? to : fallbackVariant,
+        config,
+        onStart,
+        onRest,
+    });
+
+    return [ref, spring];
+};
